53ac8fabaac110c46dfbb008f0695406029fb126
MANOJJKUMARR <Manojj5613@gmail.com>
2024-11-24 18:47:26 +0800

    Lab 5a_q1.php

--- lab5a_q1.php
+++ lab5a_q1.php
@@ -3,45 +3,37 @@
 <head>
     <title>Lab 5a Q1</title>
 </head>
+
 <body>
-    <?php 
-        $name = "Nur Ariffin Mohd Zin";
-    ?>
-    <?php 
-        $matric = "DI220043";
-    ?>
-    <?php 
-        $course = "Bis";
-    ?>
-    <?php 
-        $year = "3";
-    ?>
-    <?php 
-        $address = "No 8, Jalan Tembikai 23,Taman Kota Masai, 81700 Pasir Gudang, Johor";
+    <?php
+    $name = "Nur Ariffin Mohd Zin";
+    $matric_number = "DI220089"; 
+    $course = "Bachelor of Computer Science (Information Security) with Honours";
+    $year_of_study = "3rd Year"; 
+    $address = "No 3, Jalan Permas, Pasir Gudang, Johor"; 
     ?>
 
-    <table>
+    <table border="2" cellpadding="5" cellspacing="0">
         <tr>
-            <td>Name:</td>
-            <td><?php echo "$name"; ?></td>
+            <td><b>Name</b></td>
+            <td><?php echo $name; ?></td>
         </tr>
         <tr>
-            <td>Matric Number:</td>
-            <td><?php echo "$matric"; ?></td>
+            <td><b>Matric Number</b></td>
+            <td><?php echo $matric_number; ?></td>
         </tr>
         <tr>
-            <td>Course:</td>
-            <td><?php echo "$course"; ?></td>
+            <td><b>Course</b></td>
+            <td><?php echo $course; ?></td>
         </tr>
         <tr>
-            <td>Year of study:</td>
-            <td><?php echo "$year"; ?></td>
+            <td><b>Year of Study</b></td>
+            <td><?php echo $year_of_study; ?></td>
         </tr>
         <tr>
-            <td>Address:</td>
-            <td><?php echo "$address"; ?></td>
+            <td><b>Address</b></td>
+            <td><?php echo $address; ?></td>
         </tr>
     </table>
-    
 </body>
 </html>

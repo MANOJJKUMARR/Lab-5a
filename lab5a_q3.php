fee20c85e335afdeb759c35b5313feff5f2f1a22
MANOJJKUMARR <Manojj5613@gmail.com>
2024-11-24 18:23:39 +0800

    Add files via upload

---
+++ lab5a_q3.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Lab 5a Q3</title>
+</head>
+
+<body>
+
+    <?php
+
+    function calculateArea($width, $length) {
+        return $width * $length;
+    }
+
+    $width = 4;
+    $length = 2;
+
+    $area = calculateArea($width, $length);
+
+    echo "<b>The area of a rectangle with a width of $width and $length is $area.</b>";
+    ?>
+</body>
+</html>
